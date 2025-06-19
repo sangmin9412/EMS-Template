882f197cfa35f0f0eba863aa69e47af3bac417c5
SRV_PUB_Sangmin1212 <tkdals9048@imform.co.kr>
2025-06-19 11:15:20 +0900

    feat: default settings

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,26 @@
-var gulp = require('gulp');
-var concat = require('gulp-concat');
-var inlineCss = require('gulp-inline-css');
+var gulp = require("gulp");
+var concat = require("gulp-concat");
+var inlineCss = require("gulp-inline-css");
 
 var build = "build/";
 
 var dev = "dev/";
 var paths = {
-    js: dev + "js/*.js",
-    css: dev + "css/*.css",
-    html: dev + "html/*.html"
-}
+  js: dev + "js/*.js",
+  css: dev + "css/*.css",
+  html: dev + "html/*.html",
+};
 
-gulp.task('inlineCss', function() {
-    return gulp.src(dev + 'html/index.html')
-        .pipe(inlineCss())
-        .pipe(gulp.dest(build + 'html'));
+gulp.task("inlineCss", function () {
+  return gulp
+    .src([`${dev}html/index.html`])
+    .pipe(inlineCss())
+    .pipe(gulp.dest(build + "html"));
 });
 
-gulp.task('watch', function() {
-    gulp.watch(paths.css, gulp.series(['inlineCss']));
-    gulp.watch(paths.html, gulp.series(['inlineCss']));
+gulp.task("watch", function () {
+  gulp.watch(paths.css, gulp.series(["inlineCss"]));
+  gulp.watch(paths.html, gulp.series(["inlineCss"]));
 });
 
-gulp.task('default', gulp.series(['inlineCss', 'watch']));
+gulp.task("default", gulp.series(["inlineCss", "watch"]));
